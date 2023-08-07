2d38c7868bc0b52c3463f9f8577ca3a526f84693
Eva Nikolaieva <draculahgirl@gmail.com>
2023-08-07 19:26:13 +0200

    update

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -5789,7 +5789,7 @@ const MovieCard = ({ movie, onMovieClick })=>{
         onClick: ()=>{
             onMovieClick(movie);
         },
-        children: movie.title
+        children: movie.Title
     }, void 0, false, {
         fileName: "src/components/movie-card/movie-card.jsx",
         lineNumber: 5,
