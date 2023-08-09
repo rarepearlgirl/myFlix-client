ef7ea49e1b0bf007e30ec4212a6ba6ae1e433ade
Eva Nikolaieva <draculahgirl@gmail.com>
2023-08-09 14:59:02 +0200

    create folders and files

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+
+export const MainView = () => {
+  const [movies, setMovies] = useState([]);
+
+  // state changes for selected movies
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch('https://movie-api-uahq.onrender.com/movies')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data);
+      })
+      .catch((error) => console.error('Error:', error)); // Catch and log any errors
+
+  }, []);
+
+  if (selectedMovie) {
+    return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
+  }
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
+  return (
+    <div>
+      {movies.map((movie, i) => (
+        <MovieCard key={i}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+    </div>
+  );
+}
---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+export const MovieCard = ({ movie, onMovieClick }) => {
+  return (
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.Title}
+    </div>
+  );
+};
+
+//PropTypes conditions to return MovieCard statement in main-view.jsx
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
